1a00c70c0878f01d3b046e5621a473eeaa7e66a3
Utkarshalways <utkarshshrma08@gmail.com>
2025-03-26 23:11:54 +0530

    Week-02

---
+++ IntegrityConstraints.sql
@@ -0,0 +1,92 @@
+use MyDatabase;
+
+SELECT * from sys.tables
+
+
+
+-- Domain Integrity Constraints
+
+CREATE TABLE Employees(
+	empId INT PRIMARY KEY,
+	name VARCHAR(40),
+	age INT CHECK (age BETWEEN 18 AND 64), -- Restricts the age between 18 and 65
+	email VARCHAR(100) UNIQUE 
+	);
+
+	INSERT INTO Employees VALUES(1,'utkarsh',2,'utkarsh.sharma@intimetec.com');
+
+	SELECT * FROM Employees;
+
+	DROP TABLE Employees;
+
+
+	-- Entity Integrity Constraints
+
+	INSERT INTO Employees VALUES(1,'utkarsh',22,'utkarsh.sharma@intimetec.com');
+
+	INSERT INTO Employees VALUES(1,'Jainam',22,'jainam.j@intimetec.com');
+	 
+	 -- It will throw Error as the empId(Primary Key) is same for utkarsh and jainam 
+
+
+	 -- REFERENTIAL INTERGRITY CONSTRAINT
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    Name VARCHAR(50)
+	);
+
+INSERT INTO Customers VALUES (1,'utkarsh'),(2,'Sumit');
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT NOT NULL,
+    OrderDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) -- Ensures valid reference
+);
+
+INSERT INTO Orders VALUES (1,1,'2025-03-26');
+SELECT * FROM Orders;
+INSERT INTO Orders VALUES (2,31,'2025-01-01'); -- WILL GIVE ERROR 
+
+-- CustomerID is not present in the Customers TABLE
+DROP TABLE Customers;
+DROP TABLE Orders;
+
+
+-- 4. Unique Constraint (Ensuring Unique Emails)
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    Email VARCHAR(100) UNIQUE, -- Prevents duplicate emails
+    Username VARCHAR(50) UNIQUE -- Prevents duplicate usernames
+);
+
+INSERT INTO Users (UserID, Email, Username) VALUES (1, 'user1@example.com', 'user1');
+
+INSERT INTO Users (UserID, Email, Username) VALUES (1, 'user2@example.com', 'user1');
+
+DROP TABLE Users;
+
+
+
+-- 5. Not Null Constraint
+
+CREATE TABLE Employees_NotNull (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL, -- Name cannot be NULL
+    Salary DECIMAL(10,2) NOT NULL -- Salary must have a value
+);
+
+INSERT INTO Employees_NotNull (EmployeeID, Name, Salary) VALUES (1, 'Bob', NULL);
+
+
+-- 6. Check Constraint (Salary Must be Greater than 0)
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Price DECIMAL(10,2) CHECK (Price > 0) -- Price must be positive
+);
+
+INSERT INTO Products (ProductID, Name, Price) VALUES (1, 'Laptop', -1200);
+
+
+
---
+++ Operators.sql
@@ -0,0 +1,127 @@
+use MyDatabase;
+
+
+SELECT 10 + 5;
+
+SELECT 10 - 5;
+
+SELECT 10 * 5; 
+
+SELECT 10 / 5;
+
+SELECT 10 % 3;
+
+
+
+-- -> 15
+-- -> 5
+-- -> 25
+-- -> 2
+-- -> 1
+
+
+
+use MyDatabase;
+
+
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Department VARCHAR(50),
+    Salary DECIMAL(10,2)
+);
+
+INSERT INTO Employees (EmployeeID, Name, Age, Department, Salary) VALUES
+(1, 'Alice', 30, 'HR', 50000),
+(2, 'Bob', 25, 'IT', 60000),
+(3, 'Charlie', 28, 'Finance', 55000),
+(4, 'David', 35, 'HR', 70000),
+(5, 'Eve', 40, 'IT', 80000),
+(6, 'Frank', 22, 'Finance', 45000),
+(7, 'Grace', 29, 'HR', 50000),
+(8, 'Hannah', 32, 'IT', 75000),
+(9, 'Ian', 27, 'Finance', 52000),
+(10, 'Jack', 24, 'HR', 48000);
+
+
+-- EQUALS 
+
+SELECT * FROM Employees WHERE Salary = 50000;
+
+-- NOT EQUALS
+
+SELECT * FROM Employees WHERE Salary <> 50000;
+SELECT * FROM Employees WHERE Salary != 50000;
+
+-- Greater Than ( > )
+
+SELECT * FROM Employees WHERE Salary > 60000;
+
+-- LESS Than ( < )
+
+SELECT * FROM Employees WHERE Salary < 60000;
+
+-- GREATER Than Equals To
+
+SELECT * FROM Employees WHERE Salary >= 55000;
+
+
+-- LESS Than Equals To
+
+SELECT * FROM Employees WHERE Salary <= 50000;
+
+-- BETWEEN 
+
+SELECT * FROM Employees WHERE Salary BETWEEN 50000 AND 70000;
+
+-- LIKE 
+
+SELECT * FROM Employees WHERE Name LIKE 'A%';
+
+--  IN (Matching a Set of Values)
+
+SELECT * FROM Employees WHERE Department IN ('IT', 'HR');
+
+
+
+
+
+-- SQL LOGICAL QUERY
+
+-- AND 
+SELECT * FROM Employees WHERE Age > 25 AND Salary > 50000;
+
+-- OR 
+SELECT * FROM Employees WHERE Department = 'IT' OR Department = 'HR';
+
+-- NOT
+SELECT * FROM Employees WHERE NOT Age = 30;
+
+
+
+-- PAGINATION QUERIES
+
+-- TOP 
+SELECT TOP 5 * FROM Employees ORDER BY Salary DESC;
+
+-- ORDER BY
+SELECT * FROM Employees ORDER BY age; 
+
+-- OFFSET FETCH
+SELECT * FROM Employees ORDER BY Salary DESC OFFSET 2 ROWS
+SELECT * FROM Employees ORDER BY Salary DESC OFFSET 2 ROWS FETCH NEXT 2 ROWS ONLY
+
+
+
+--Which QUERY is Right ? about fetching First 5 top rows
+SELECT * FROM Employees ORDER BY EmployeeID OFFSET 5 ROWS;
+SELECT TOP 5 * FROM Employees;
+
+
+-- How do you retrieve the top 3 highest-paid employees?
+SELECT TOP 3 * FROM Employees ORDER BY Salary DESC;
+SELECT * FROM Employees ORDER BY Salary DESC OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY
+
+
+DROP TABLE Employees;
