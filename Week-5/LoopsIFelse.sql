3e119610723a6ce04756bb1fe34f7fcebccce4ec
utkarshalways <utkarshshrma08@gmail.com>
2025-04-17 17:18:13 +0530

    Update: Try Catch Block added

--- Week-5/LoopsIFelse.sql
+++ Week-5/LoopsIFelse.sql
@@ -32,3 +32,57 @@ ELSE
 END
 
 
+
+
+
+BEGIN TRANSACTION;
+
+INSERT INTO Sales VALUES ('utkarsh','March',12000)
+INSERT INTO Sales VALUES ('utkarsh','March',12000)
+INSERT INTO Sales VALUES ('utkarsh','March',12000)
+INSERT INTO Sales VALUES ('utkarsh','March',12000)
+
+
+
+SELECT * FROM Sales
+
+ROLLBACK;
+COMMIT;
+SAVEPOINT;
+
+
+
+-- Create demo table
+CREATE TABLE BankAccounts (
+    AccountID INT PRIMARY KEY,
+    AccountHolder VARCHAR(50),
+    Balance DECIMAL(10,2)
+);
+
+-- Insert test data
+INSERT INTO BankAccounts VALUES
+(1, 'Ravi', 5000),
+(2, 'Amit', 3000);
+
+
+
+BEGIN TRANSACTION;
+
+BEGIN TRY
+    -- Debit from Ravi
+    UPDATE BankAccounts 
+    SET Balance = Balance - 1000 
+    WHERE AccountID = 1;
+
+    UPDATE BankAccounts 
+    SET Balance = Balance + 1000 
+    WHERE AccountID = 2;
+
+    COMMIT; -- If both succeed
+    PRINT 'Transfer Successful';
+END TRY
+BEGIN CATCH
+    ROLLBACK; -- Undo everything if any error occurs
+    PRINT 'Transaction Failed';
+END CATCH;
+
